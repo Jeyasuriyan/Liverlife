b36c8b7aa206bfdd8151a6fe582c395883bd39bd
Jeyasuriyan <jeyasuriyan46@gmail.com>
2021-09-30 19:05:22 +0530

    test

--- src/test/java/baseUtils/Hook_s.java
+++ src/test/java/baseUtils/Hook_s.java
@@ -1,23 +1,13 @@
 package baseUtils;
 
-import baseUtils.Base;
 import io.cucumber.java.*;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.io.File;
-import java.io.IOException;
-
-public class Hooks extends Base {
+public class Hook_s extends Base {
 
     private final Base base;
 
-    public Hooks(Base base) {
+    public Hook_s(Base base) {
         this.base = base;
     }
 
